6358fcf987c9de19214cae40fc8a26b7ae320e0a
cbfont01 <cbfont01@gmail.com>
2018-09-17 20:17:43 -0500

    Correct Initial Board

--- GUI.py
+++ GUI.py
@@ -18,12 +18,6 @@ class Player:
             self.ai = False
 
 
-# TODO: change grid
-# Changes board, but doesn't change grid currently
-def manual_move(i, j, player):
-    board[i][j] = player.piece
-
-
 def on_click(i,j,event):
     global counter
     global curr_p
@@ -46,21 +40,22 @@ curr_p = p1
 # Main Program Board/GUI Setup
 
 # Setup of initial 4 squares
+board[3,3] = p1.piece
 board[4,4] = p1.piece
-board[5,5] = p1.piece
-board[4,5] = p2.piece
-board[5,4] = p2.piece
-# manual_move(4,4,p1)
-# manual_move(5,5,p1)
-# manual_move(4,5,p2)
-# manual_move(5,4,p2)
+board[3,4] = p2.piece
+board[4,3] = p2.piece
 
 # Sets up initial GUI. It is an array of buttons.
 root = tk.Tk()
 for i, row in enumerate(board):
     for j, column in enumerate(row):
         # relief adds an edge to the button so you can see individual squares
-        L = tk.Label(root,text='     ',bg='green',relief='sunken')
+        if (i==3 and j==3) or (i==4 and j==4):
+            L = tk.Label(root, text='     ', bg='white', relief='sunken')
+        elif (i==3 and j==4) or (i == 4 and j == 3):
+            L = tk.Label(root, text='     ', bg='black', relief='sunken')
+        else:
+            L = tk.Label(root,text='     ',bg='green',relief='sunken')
         L.grid(row=i,column=j)
         # makes GUI change when clicked
         L.bind('<Button-1>',lambda e,i=i,j=j: on_click(i,j,e))
